b51fa924cc7bb87d1ae5c1ee333f3916157d69f1
massimo12345 <rboccunigodfrey@rockyhill.org>
2017-07-10 21:10:16 -0400

    Added file

---
+++ TransitionManager.swift
@@ -0,0 +1,35 @@
+//
+//  TransitionManager.swift
+//  im_getting_arrested
+//
+//  Created by Quadrant 2 on 6/29/17.
+//  Copyright © 2017 Quadrant2. All rights reserved.
+//
+
+import UIKit
+
+class TransitionManager: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
+        let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
+        let finalFrameForVC = transitionContext.finalFrame(for: toViewController)
+        let containerView = transitionContext.containerView
+        let bounds = UIScreen.main.bounds
+        toViewController.view.frame = finalFrameForVC.offsetBy(dx: bounds.size.width, dy: 0)
+        containerView.addSubview(toViewController.view)
+        
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: .curveLinear, animations: {
+            fromViewController.view.alpha = 0.5
+            toViewController.view.frame = finalFrameForVC
+        }, completion: {
+            finished in
+            transitionContext.completeTransition(true)
+            fromViewController.view.alpha = 1.0
+        })
+    }
+}
